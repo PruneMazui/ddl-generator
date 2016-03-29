64a92333b26de6eb1e2365b54bf8ff907082cc28
ko_tanaka <ko_tanaka@ap-com.co.jp>
2016-03-29 18:59:27 +0900

    ディレクトリ構成作成

---
+++ src/DdlGenerator.php
@@ -0,0 +1,18 @@
+<?php
+namespace PruneMazui\DdlGenerator;
+
+class DdlGenerator
+{
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+}
---
+++ tests/DdlGenerateTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace PruneMazui\DdlGeneratorTest;
+
+use PruneMazui\DdlGenerator\DdlGenerator;
+
+class DdlGenerateTest extends \PHPUnit_Framework_TestCase
+{
+    private $config;
+
+    protected function setUp()
+    {
+        $this->config = array(
+            'filename' => __DIR__ . '/files/db_specifications.xlsx'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function runTest()
+    {
+        $generator = new DdlGenerator($this->config);
+
+        $this->assertTrue($generator instanceof DdlGenerator);
+    }
+}
